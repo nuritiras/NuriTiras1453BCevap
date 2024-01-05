a9cc77d0099032da064747be9dc9007445a3a5ee
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2024-01-05 17:44:49 +0300

    Proje dosyası ekle.

---
+++ NuriTiras1453B/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuriTiras1453B
+{
+    public class Suleyman
+    {
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public byte yas;
+        public double boy, agirlik;
+
+        public virtual double VcutKitleIndeksi()
+        {
+            return agirlik / (boy * boy);
+        }
+    }
+    public class Nuri : Suleyman
+    {
+        public string OkuduguBolum;
+        public bool Durum;
+        public Nuri()
+        {
+            Console.WriteLine("Bilişim Teknolojileri öğrencisi her zaman fit bir vücuda sahiptir.");
+        }
+        public override double VcutKitleIndeksi()
+        {
+            return 20.85;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Suleyman suleyman= new Suleyman();
+            suleyman.isim = "Süleyman";
+            suleyman.yas = 74;
+            suleyman.boy = 1.80;
+            suleyman.agirlik = 80.5;
+            Console.WriteLine("Vücut Kitle İndeks Değeri={0}", suleyman.VcutKitleIndeksi());
+        }
+    }
+}
